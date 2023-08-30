374fce19e3c85ef5e9b3494045db413668db8e72
gtaiyou24 <gtaiyou24@gmail.com>
2023-08-30 22:48:55 +0900

    delete: requests

--- app/application/page/page_application_service.py
+++ app/application/page/page_application_service.py
@@ -1,13 +1,9 @@
-import time
-
-import requests
 from injector import singleton, inject
 
 from application.page import DownloadedPageDpo
-from domain.model.page.html import HTML, CharacterCode
 from domain.model.user_agent import UserAgent
 from domain.model.user_agent.device import Device
-from domain.model.page import PageService, Page, HttpStatus
+from domain.model.page import PageService
 from domain.model.url import URL
 
 
@@ -22,16 +18,6 @@ class PageApplicationService:
         device = Device.value_of(a_device)
         user_agent = UserAgent.random(device)
 
-        response: requests.Response = requests.get(url.value, headers={'User-Agent': user_agent.value}, timeout=(10.0, 20.0))
-
-        if response.ok:
-            page = Page(
-                URL(response.url),
-                HttpStatus.value_of(response.status_code),
-                HTML(response.text, CharacterCode.value_of(response.apparent_encoding))
-            )
-        else:
-            time.sleep(1.5)
-            page = self.__page_service.download(url, user_agent)
+        page = self.__page_service.download(url, user_agent)
 
         return DownloadedPageDpo(page)
