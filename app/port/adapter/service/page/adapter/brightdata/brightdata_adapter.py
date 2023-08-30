0de4bfc5a7b391aec71444fd1cdf142b026d0508
gtaiyou24 <gtaiyou24@gmail.com>
2023-08-30 22:51:10 +0900

    feat: bright data selenium でスクロール

--- app/port/adapter/service/page/adapter/brightdata/brightdata_adapter.py
+++ app/port/adapter/service/page/adapter/brightdata/brightdata_adapter.py
@@ -1,9 +1,10 @@
 import os
 
 import requests
-from selenium.common import TimeoutException
-from selenium.webdriver import Remote, ChromeOptions
+from selenium.common import TimeoutException, JavascriptException
+from selenium.webdriver import Remote, ChromeOptions, Keys
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from selenium.webdriver.common.by import By
 
 from domain.model.page import Page, HttpStatus
 from domain.model.page.html import CharacterCode, HTML
@@ -23,6 +24,14 @@ class BrightDataAdapter(PageAdapter):
         with Remote(self.__remote_connection, options=self.__options) as web_driver:
             try:
                 web_driver.get(url.value)
+
+                try:
+                    web_driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                except JavascriptException as e:
+                    # NOTE: 上記の方法でスクロールできない場合
+                    web_driver.find_element(By.TAG_NAME, "body").click()
+                    web_driver.find_element(By.TAG_NAME, "body").send_keys(Keys.PAGE_DOWN)
+
                 response = requests.get(web_driver.current_url, headers={'User-Agent': user_agent.value})
             except TimeoutException:
                 raise SystemException(ErrorCode.PAGE_TIMEOUT, 'page {} timeout.'.format(url.value))
